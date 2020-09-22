9f9f21c391c583c04cc6812007e25fec8e67bc5a
ChaeDLR <delarosa.chae@gmail.com>
2020-09-22 10:39:56 -0400

    Updated code.
    Fixed indexing bug.

--- Program.cs
+++ Program.cs
@@ -1,49 +1,65 @@
-﻿using System;
-using System.Net.Http;
+﻿// Chae DeLaRosa
+// 
+using System;
+
 
 namespace Ciphers
 {
     class Program
     {
-        static int Menu()
+        static string Menu()
         {
             Console.WriteLine("Select Cipher.");
             Console.WriteLine("1. Cesars cipher.");
-            Console.WriteLine("2. Exit the program.");
+            Console.WriteLine("Enter 'exit' to quit the program.");
             string usersSelection = Console.ReadLine();
-            return Convert.ToInt32(usersSelection);
+            return usersSelection;
         }
+
+        // Inputs: A string to encrypted or decrypted,
+        // The int key used in the encryption method,
+        // And the int of the encryption or decryption selection
+
+        // Outputs: Encrypted or decrypted string
         static string CaesarsCipher(string message, int key, int encryptionflag)
         {
-            string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.,?! ";
-
+            // string of characters used for the encryption and decryption
+            string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz !?.,";
+            // Found this as a way to append to a string repeatedly 
             System.Text.StringBuilder encryptedMessage = new System.Text.StringBuilder("");
 
+            // perform encryption or decryption based on the encryptionflag input
             switch (encryptionflag)
             {
+                // encryption case
                 case 1:
+                    // loop through the message so we can work with each character individually
                     foreach (char letter in message)
                     {
+                        // If the character in the message is in our character list
                         if (characters.Contains(letter))
                         {
+                            // for encryption we want to take the index of the char and add the key
                             int characterIndex = characters.IndexOf(letter) + key;
-                            if (characterIndex >= characters.Length)
+                            // If the key is massive we need to be able to get it into a workable index
+                            while (characterIndex >= characters.Length)
                             {
                                 characterIndex -= characters.Length;
                             }
-                            
+                            // append our encypted char to our ciphertext
                             encryptedMessage.Append(characters[characterIndex]);
                         }
                     }
                     break;
-
+                // decryption case
+                // same thing as encryption just backwards
                 case 2:
                     foreach (char letter in message)
                     {
                         if (characters.Contains(letter))
                         {
                             int characterIndex = characters.IndexOf(letter) - key;
-                            if (characterIndex <= 0)
+                            while (characterIndex <= 0)
                             {
                                 characterIndex += characters.Length;
                             }
@@ -56,19 +72,23 @@ namespace Ciphers
                     Console.WriteLine("Error");
                     break;
             }
-
             return Convert.ToString(encryptedMessage);
         }
+
+
         static void Main(string[] args)
         {
+            // Keep our program running until the user stops it
             bool running = true;
             while (running)
             {
-                int menuSelection = Menu();
-
+                // grab our users selection
+                string menuSelection = Menu().ToLower();
+                // Decide what to do with it
                 switch (menuSelection)
                 {
-                    case 1:
+                    // caesars cipher
+                    case "1":
                         Console.WriteLine("Make a selection.");
                         Console.WriteLine("1. Encrypt");
                         Console.WriteLine("2. Decrypt");
@@ -77,7 +97,9 @@ namespace Ciphers
                         string message = Console.ReadLine();
                         Console.WriteLine("Enter a key.");
                         int key = Convert.ToInt32(Console.ReadLine());
+                        // Execute cipher
                         string alteredMessage = CaesarsCipher(message, key, encryptionSelection);
+                        // Decide what to print to the user depending on their selection
                         if (encryptionSelection == 1)
                         {
                             Console.WriteLine($"Encrypted message: \"{alteredMessage}\"");
@@ -87,13 +109,13 @@ namespace Ciphers
                             Console.WriteLine($"Decrypted message: \"{alteredMessage}\"");
                         }
                         break;
-                    case 2:
+                    case "exit":
+                        Console.WriteLine("Quiting program...");
                         running = false;
                         break;
                     default:
                         Console.WriteLine("Invalid input.");
                         break;
-
                 }
             }
         }
